b4b660e4fce4ad3deb7066475108f643e54833d2
SpikeHD <spikegdofficial@gmail.com>
2022-12-30 22:28:28 -0800

    add controls

---
+++ source/controls.cpp
@@ -0,0 +1,30 @@
+#include "./controls.h"
+
+u32 kdr() {
+	return keysHeld();
+}
+
+/* Controls section */
+bool btn_up() {
+	return kdr() & KEY_UP;
+}
+
+bool btn_down() {
+	return kdr() & KEY_DOWN;
+}
+
+bool btn_left() {
+	return kdr() & KEY_LEFT;
+}
+
+bool btn_right() {
+	return kdr() & KEY_RIGHT;
+}
+
+bool btn_lbump() {
+  return kdr() & KEY_L;
+}
+
+bool btn_rbump() {
+  return kdr() & KEY_R;
+}
---
+++ source/controls.h
@@ -0,0 +1,13 @@
+#include <nds.h>
+
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+bool btn_up();
+bool btn_down();
+bool btn_left();
+bool btn_right();
+bool btn_lbump();
+bool btn_rbump();
+
+#endif
